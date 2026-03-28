a39f4feab6f703f472c0d97d927c8b3fd9c0fdcd
Sebastian (seprog) Poppenborg <9268780+seprog@users.noreply.github.com>
2026-03-28 15:30:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,30 @@
 SETUP_PATH=$PWD
 
-# Install paru
-sudo pacman --sync --needed --noconfirm base-devel
+
+# Install package manager (paru)
+sudo pacman --sync --needed --noconfirm base-devel git
 [[ -d /tmp/paru ]] && sudo rm --recursive /tmp/paru
+
 git clone https://aur.archlinux.org/paru.git /tmp/paru
 cd /tmp/paru
 makepkg --syncdeps --install --needed --noconfirm
 cd $SETUP_PATH
+
 [[ -d /tmp/paru ]] && sudo rm --recursive /tmp/paru
+
 
 # Install packages
 paru -S --color=auto --needed --noconfirm $(grep "^[^#]" $SETUP_PATH/packages)
 
-# Configuration
-sudo systemctl enable --now NetworkManager.service
+
+# Configure installation
 cp --recursive $SETUP_PATH/home/. ~
+
+sudo systemctl enable --now NetworkManager.service
+sudo systemctl enable --now cockpit.socket
+
 sudo chsh -s $(which fish) $USER
 
+
 # Activate
-sudo reboot
+read -p "Reboot now? [y/N] " -n1 && [[ $REPLY =~ ^[Yy]$ ]] && sudo reboot
